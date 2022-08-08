d36d9ae07d6516538e93e906c5f482c4a5f4c01f
zhangshixin <mrzsxofanhui@163.com>
2022-08-08 18:48:01 +0800

    feat: 增加组件组件

---
+++ src/components/calender/XiaobaiCalender.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="w-full h-full">
+    <div class="w-full h-[70px] bg-gray-50 flex items-center">
+      <p class="text-[24px] ml-[10px]">{{ poetry }}</p>
+    </div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { onMounted, ref } from "vue";
+
+const poetry = ref("");
+
+function getPoetry() {
+  const jinrishici = require("jinrishici");
+  console.log(jinrishici);
+  jinrishici.load((result: { data: { content: string } }) => {
+    poetry.value = result.data.content;
+  });
+}
+
+onMounted(() => {
+  getPoetry();
+});
+</script>
--- src/layout/PageLayout.vue
+++ src/layout/PageLayout.vue
@@ -1,3 +1,12 @@
+<!--
+ * @Author: zhangshixin mrzsxofanhui@163.com
+ * @Date: 2022-08-08 08:31:12
+ * @LastEditors: zhangshixin mrzsxofanhui@163.com
+ * @LastEditTime: 2022-08-08 08:39:45
+ * @FilePath: \xiaobai-calendar\src\layout\PageLayout.vue
+ * @Description: 
+ * @Signature: No Bugs No Overtime
+-->
 <template>
   <div class="flex h-full">
     <div
@@ -6,7 +15,7 @@
       <NavigationMenu />
     </div>
     <div class="h-full bg-gray-500 animationDelay" :style="calenderStyle">
-      <div class="h-full"></div>
+      <XiaobaiCalender></XiaobaiCalender>
     </div>
     <div class="h-full bg-slate-700 animationDelay" :style="todoStyle"></div>
   </div>
@@ -14,18 +23,21 @@
 
 <script lang="ts" setup>
 import NavigationMenu from "~/components/menu/NavigationMenu.vue";
-import { reactive, CSSProperties, computed } from "vue";
+import { reactive, CSSProperties, computed, ref, unref, onMounted } from "vue";
 import { px2vmin } from "~/utils/viewport";
 import { useSettingStore } from "~/stores/setting";
+import XiaobaiCalender from "~/components/calender/XiaobaiCalender.vue";
 
 const settingStore = useSettingStore();
 
+const clientWidth = ref(window.document.body.clientWidth)
+
 const calenderStyle = computed<CSSProperties>(() => {
-  const clientWidth = window.document.body.clientWidth
+  clientWidth.value = window.document.body.clientWidth
   const pxNum = new Map([
     ["true", 0],
-    ["false", 740/800*clientWidth],
-    ["auto", 450/800*clientWidth],
+    ["false", 740/800*unref(clientWidth)],
+    ["auto", 450/800*unref(clientWidth)],
   ]).get(settingStore.todo.toString()) as number;
   return {
     width: px2vmin(pxNum) + "vmin",
@@ -33,14 +45,18 @@ const calenderStyle = computed<CSSProperties>(() => {
 });
 
 const todoStyle = computed<CSSProperties>(() => {
-  const clientWidth = window.document.body.clientWidth
+  clientWidth.value = window.document.body.clientWidth
   const pxNum = new Map([
-    ["true", 740/800*clientWidth],
+    ["true", 740/800*unref(clientWidth)],
     ["false", 0],
-    ["auto", 290/800*clientWidth],
+    ["auto", 290/800*unref(clientWidth)],
   ]).get(settingStore.todo.toString()) as number;
   return {
     width: px2vmin(pxNum) + "vmin",
   };
 });
+
+onMounted(()=> {
+  clientWidth.value = window.document.body.clientWidth
+})
 </script>
